c2d45a60568266428b441ed9ab8165d5580da0f7
Mathias Rasmussen <49396726+GuRLiG@users.noreply.github.com>
2025-12-12 09:13:27 +0100

    Updated Consumers to follow the new pattern

--- Marketplace/Consumers/CreateBookConsumer.cs
+++ Marketplace/Consumers/CreateBookConsumer.cs
@@ -2,50 +2,22 @@ using System.Text.Json;
 using Marketplace.Business.Interfaces;
 using Marketplace.Contracts.Commands;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Shared;
 
 namespace Marketplace.Consumers;
 
-public class CreateBookConsumer : BackgroundService
+public class CreateBookConsumer : BaseConsumer<CreateBook>
 {
-    private readonly IServiceScopeFactory _serviceScopeFactory;
-    private readonly IConsumer _consumer; // the RabbitMQ consumer we built
-    private const string QueueName = "marketplace.create-book"; // Should inpmlement a better way to manage queue names such as a config file
+    protected override string QueueName => "marketplace.create-book";
 
     public CreateBookConsumer(IServiceScopeFactory serviceScopeFactory, IConsumer consumer)
+        : base(serviceScopeFactory, consumer)
     {
-        _serviceScopeFactory = serviceScopeFactory;
-        _consumer = consumer;
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task HandleMessageAsync(CreateBook command, IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
-        await _consumer.StartAsync(
-            queueName: QueueName,
-            handler: async (message, ct) =>
-            {
-                using var scope = _serviceScopeFactory.CreateScope();
-                var createBookLogic = scope.ServiceProvider.GetRequiredService<ICreateBookLogic>();
-
-                var dto = JsonSerializer.Deserialize<CreateBook>(message)!;
-                await createBookLogic.CreateBook(dto, ct);
-            },
-            cancellationToken: stoppingToken
-        );
-
-        try
-        {
-            await Task.Delay(Timeout.Infinite, stoppingToken);
-        }
-        catch (OperationCanceledException)
-        {
-        }
-    }
-
-    public override async Task StopAsync(CancellationToken cancellationToken)
-    {
-        await _consumer.DisposeAsync(); // stop consumer properly
-        await base.StopAsync(cancellationToken);
+        var createBookLogic = serviceProvider.GetRequiredService<ICreateBookLogic>();
+        await createBookLogic.CreateBook(command, cancellationToken);
     }
 }
